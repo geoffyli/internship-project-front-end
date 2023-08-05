9754b9009a9a6ba19fa4c4c8f289a0809a41cb46
Geoff Li <geoff.yulong.li@gmail.com>
2023-08-06 00:41:18 +0800

    Import components

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import  '@/assets/styles.scss'
+import PrimeVue from 'primevue/config';
 
 const app = createApp(App)
 
+app.use(PrimeVue, {ripple: true})
 app.use(router)
 
 app.mount('#app')
